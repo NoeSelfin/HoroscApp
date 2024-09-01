5824baf3f2ef2881b8bcffd4c4548ae15b256a6a
NoeSelfin <noemiselfin@gmail.com>
2024-09-01 16:38:20 +0200

    initial commit

---
+++ app/src/main/java/com/NoeDev/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,27 @@
+package com.NoeDev.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.NoeDev.horoscapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
